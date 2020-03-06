a74a701612155662384a20b73181da11871b4e4e
kamal <kamal777682>
2020-03-06 17:53:55 -0500

    [kamal]  initial view Controller

---
+++ C0777682_MidTerm_MAD3115W2020/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  C0777682_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-06.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var txtEmail: UITextField!
+    
+    @IBOutlet var txtPassword: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
