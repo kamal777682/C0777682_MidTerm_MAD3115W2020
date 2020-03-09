746b8cb745947502399f8e5112fa2545c03af70f
kamal <kamal777682>
2020-03-09 17:46:57 -0400

    [kamal] Hydro class

---
+++ C0777682_MidTerm_MAD3115W2020/Delegates/Hydro.swift
@@ -0,0 +1,24 @@
+//
+//  Hydro.swift
+//  C0777682_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-09.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Hydro
+{
+  var agencyName : String
+   var unitConsumed : Int
+   var rate : Double
+   
+   init(agencyName : String,unitConsumed : Int,rate : Double)
+   {
+       self.agencyName = agencyName
+       self.unitConsumed = unitConsumed
+       self.rate = rate
+     
+   }
+}
