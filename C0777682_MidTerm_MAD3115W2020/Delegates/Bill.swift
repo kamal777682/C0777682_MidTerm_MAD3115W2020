9ddaf3fdc60f66c990ca9961045ce8b941a1c2b5
kamal <kamal777682>
2020-03-09 17:50:04 -0400

    [kamal] Bill Class

---
+++ C0777682_MidTerm_MAD3115W2020/Delegates/Bill.swift
@@ -0,0 +1,54 @@
+//
+//  Bill.swift
+//  C0777682_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-09.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+enum BillType {
+    case Mobile
+    case Internet
+    case Hydro
+}
+
+class Bill : IDisplay
+{
+    var billId :  String
+    var billDate : String
+    var billType : BillType
+    var totalBill :  Double = 0.0
+    
+       
+    init(billId : String,billDate : String,billType : BillType)//totalBill :  Double)
+    {
+        self.billId = billId
+        self.billDate = billDate
+        self.billType = billType
+        //self.totalBill = totalBill
+      
+    }
+   
+    func display()
+    {
+        print("BIll Id              : \(self.billId)")
+        print("Bill Date            : \(self.dateFormat(string: billDate))")
+        print("Bill Type            : \(self.billType)")
+      
+    }
+    
+    func dateFormat(string : String)-> String
+        
+    {
+        let getdate = DateFormatter()
+        getdate.dateFormat = "dd/MM/yyyy"
+        
+        let setdate = DateFormatter()
+        setdate.dateFormat = "EEEE,d MMMM ,yyyy"
+        let date : Date? =  getdate.date(from: billDate)
+        return setdate.string(from: date!)
+          
+    }
+}
---
+++ C0777682_MidTerm_MAD3115W2020/Delegates/Mobile.swift
@@ -0,0 +1,31 @@
+//
+//  Mobile.swift
+//  C0777682_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-09.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Mobile
+{
+    var manufacturerName : String
+    var mobileNumber : String
+    var mobilePlan :String
+    var internetGBUsed : Int
+    var minuteUsed : Int
+    var planRate :  Double
+    var internetRate : Double
+    
+    init(billId :String, billDate : String ,billType : BillType,  manufacturerName : String, mobileNumber : String, mobilePlan : String , internetGBUsed : Int, minuteUsed: Int,planRate : Double,internetRate : Double)
+    {
+        self.manufacturerName   = manufacturerName
+        self.mobileNumber = mobileNumber
+        self.mobilePlan = mobilePlan
+        self.internetGBUsed = internetGBUsed
+        self.minuteUsed = minuteUsed
+        self.planRate = planRate
+        self.internetRate = internetRate
+        super.init(billId: billId, billDate : billDate, billType: billType)
+    }
