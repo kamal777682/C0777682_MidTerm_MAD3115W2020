2a73e7f8839c5486ab8a65099739ab2d7b3083b7
kamal <kamal777682>
2020-03-10 17:16:51 -0400

    hydroTableViewCell

---
+++ C0777682_MidTerm_MAD3115W2020/HydroTableViewCllTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HydroTableViewCllTableViewCell.swift
+//  C0777682_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-10.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class HydroTableViewCllTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
