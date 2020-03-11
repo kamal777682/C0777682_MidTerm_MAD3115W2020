2f3238bbea9ea683415195843c758a8130b7a117
kamal <kamal777682>
2020-03-11 17:34:51 -0400

    working on data storage

---
+++ C0777682_MidTerm_MAD3115W2020/Internet.swift
@@ -0,0 +1,20 @@
+//
+//  Internet.swift
+//  C0777682_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-11.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Internet : Bill{
+   
+  var billType : String = "Internet"
+  var internetProvider : String
+  var internetGBUsed : Int
+  init(billId: String, billDate: String,internetProvider : String, internetGBUsed : Int) {
+    self.internetProvider = internetProvider
+    self.internetGBUsed = internetGBUsed
+    super.init(billId: billId, billDate: billDate)
+  }
+}
--- C0777682_MidTerm_MAD3115W2020/Mobile.swift
+++ C0777682_MidTerm_MAD3115W2020/Mobile.swift
@@ -10,14 +10,14 @@ import Foundation
 class Mobile : Bill{
    
   var billType : String = "Mobile"
-  var mobileManufacturerName : String
-  var planName : String
+  var manufacturerName : String
+  var mobilePlan : String
   var mobileNumber : String
   var internetGBUsed : Int
   var minuteUsed : Int
-  init(billId: String, billDate: String, mobileManufacturerName : String, planName : String, mobileNumber : String, internetGBUsed : Int, minuteUsed : Int) {
-    self.mobileManufacturerName = mobileManufacturerName
-    self.planName = planName
+  init(billId: String, billDate: String, manufacturerName : String, mobilePlan : String, mobileNumber : String, internetGBUsed : Int, minuteUsed : Int) {
+    self.manufacturerName = manufacturerName
+    self.mobilePlan = mobilePlan
     self.mobileNumber = mobileNumber
     self.internetGBUsed = internetGBUsed
     self.minuteUsed = minuteUsed
